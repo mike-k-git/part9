feb97978ceecfe35215330befde8a02bfd846c98
mike-k-git <mikehsm@outlook.com>
2022-06-09 23:22:06 +0300

    9.9: Patientor backend, step2

---
+++ patientor/backend/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
+
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
